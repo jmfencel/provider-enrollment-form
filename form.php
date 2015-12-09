6430e3ce4d57bccfe82eacb143965c23e2196850
Jordan <jmfencel@gmail.com>
2015-12-08 19:30:43 -0500

    Fixed typo on line 12

--- form.php
+++ form.php
@@ -9,7 +9,7 @@
 	<input type="checkbox" name="indi" value="i">Individual<br>
 	<input type="checkbox" name="group" value="g">Group<br>
 	<input type="checkbox" name="grpvdr" value="gp">Group Provider<br>
-	<input type="checkbox" name="fac" value="f">Facility>
+	<input type="checkbox" name="fac" value="f">Facility
 </form>
 </body>
 </html>
