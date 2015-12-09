0ca88d66184bc607897617e1856aab58ced78548
Jordan <jmfencel@gmail.com>
2015-12-08 21:08:54 -0500

    Adding Dropdowns

--- form.php
+++ form.php
@@ -9,7 +9,14 @@
 	<input type="checkbox" name="indi" value="i">Individual<br>
 	<input type="checkbox" name="group" value="g">Group<br>
 	<input type="checkbox" name="grpvdr" value="gp">Group Provider<br>
-	<input type="checkbox" name="fac" value="f">Facility
+	<input type="checkbox" name="fac" value="f">Facility<br>
+	Provider type: <br>
+		<select name="prov_types" multiple size="4">
+			<option value="i">Individual</option>
+			<option value="g">Group</option>
+			<option value="gp">Group Provider</option>
+			<option value="f">Facility</option>
+		</select><br>
 </form>
 </body>
 </html>
