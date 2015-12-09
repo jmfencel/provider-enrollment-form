53e7ebb4cfe0207fb063c021c483d4c474c1919e
Jordan <jmfencel@gmail.com>
2015-12-08 19:20:35 -0500

    Sample Form
    Testing out form with simple checkboxes

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Provider Enrollment Form</title>
+</head>
+<body>
+<form method="post" action="">
+	Provider type: <br>
+	<input type="checkbox" name="indi" value="i">Individual<br>
+	<input type="checkbox" name="group" value="g">Group<br>
+	<input type="checkbox" name="grpvdr" value="gp">Group Provider<br>
+	<input type="checkbox" name="fac" value="f">Facility>
+</form>
+</body>
+</html>
