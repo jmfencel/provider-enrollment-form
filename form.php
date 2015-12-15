037b8cdab18537ae159eb1fc8286020e7e275a4c
Jordan <jmfencel@gmail.com>
2015-12-14 22:55:12 -0500

    Form Validation
    Started to add some simple form validation along with htmlspecialchars
    to $_POST lines.

--- form.php
+++ form.php
@@ -6,8 +6,21 @@
 <body>
 <?php 
   if (isset($_POST['submit'])) {
+    $ok = true;
+
+    // check if Provider type is not selected
+    if (!isset($_POST['prov_types']) || $_POST['prov_types'] === '') {
+      $ok = false;
+    }
+    
+    // check if State is selected
+    if (!isset($_POST['state']) || $_POST['state'] === 'blank') {
+      $ok = false;
+    }
+
+    if($ok) {
   	// process form
-  	printf('Provider type: %s
+  	  printf('Provider type: %s
   		<br>Provider fist name: %s
   		<br>Provider last name: %s
   		<br>Degree: %s
@@ -27,27 +40,28 @@
   		<br>Group name: %s
   		<br>Contact name: %s
   		<br>Contact title: %s',
-  			$_POST['prov_types'],
-  			$_POST['p_first'],
-  			$_POST['p_last'],
-  			$_POST['deg'],
-  			$_POST['address'],
-  			$_POST['city'],
-  			$_POST['state'],
-  			$_POST['zip'],
-  			$_POST['county'],
-  			$_POST['phone'],
-  			$_POST['fax'],
-  			$_POST['email'],
-  			$_POST['medicaid'],
-  			$_POST['npi_i'],
-  			$_POST['npi_ii'],
-  			$_POST['bill_type'],
-  			$_POST['bill_info'],
-  			$_POST['gn'],
-  			$_POST['cn'],
-  			$_POST['ct']);
-  }
+  			htmlspecialchars($_POST['prov_types']),
+  			htmlspecialchars($_POST['p_first']),
+  			htmlspecialchars($_POST['p_last']),
+  			htmlspecialchars($_POST['deg']),
+  			htmlspecialchars($_POST['address']),
+  			htmlspecialchars($_POST['city']),
+  			htmlspecialchars($_POST['state']),
+  			htmlspecialchars($_POST['zip']),
+  			htmlspecialchars($_POST['county']),
+  			htmlspecialchars($_POST['phone']),
+  			htmlspecialchars($_POST['fax']),
+  			htmlspecialchars($_POST['email']),
+  			htmlspecialchars($_POST['medicaid']),
+  			htmlspecialchars($_POST['npi_i']),
+  			htmlspecialchars($_POST['npi_ii']),
+  			htmlspecialchars($_POST['bill_type']),
+  			htmlspecialchars($_POST['bill_info']),
+  			htmlspecialchars($_POST['gn']),
+  			htmlspecialchars($_POST['cn']),
+  			htmlspecialchars($_POST['ct']));
+      }
+    }
 ?>
 <form method="post" action="">
 	Provider type: <br>
@@ -59,12 +73,14 @@
 		</select><br>
 	Provider first name: <input type="text" name="p_first"><br>
     Provider last name: <input type="text" name="p_last"><br>
-    Degree: <select name="deg" multiple size="1">
+    Degree: <select name="deg">
+            <option value="blank"></option>
             <option value="md">MD</option>  
         </select><br>
     Address: <input type="text" name="address"><br>
     City: <input type="text" name="city"><br>
-    State: <select name="state" multiple size="1">
+    State: <select name="state">
+                    <option value="blank"></option>
                     <option value="fl">FL</option>
            </select><br>
     Zip: <input type="text" name="zip"><br>
@@ -78,6 +94,7 @@
     Type I NPI: <input type="text" name="npi_i"><br>
     Type II NPI: <input type="text" name="npi_ii"><br>
     Billing info: <select name="bill_type">
+                    <option value="blank"></option>
                     <option value="ssn">SSN</option>
                     <option value="tin">TIN</option>
 			      </select><input type="text" name="bill_info"><br>
